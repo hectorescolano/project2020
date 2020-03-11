a0c09d5b3c34c88c095a8fa442ece16b7c79faf3
HJE <hjescolano@gmail.com>
2020-03-11 11:34:28 +0800

    Added view for login_page.php

---
+++ application/views/contents/login_page.php
@@ -0,0 +1,10 @@
+<div>
+	<h1>Barangay Case System</h1>
+	<form id="login_form" name="login_form" method="post" action="">
+		<label for="login_usr">Username:</label>
+		<input type="text" id="login_usr" name="login_usr" required="">
+		<label for="login_pwd">Password:</label>
+		<input type="password" id="login_pwd" name="login_pwd" required="">
+		<button type="submit">Login</button>
+	</form>
+</div>
